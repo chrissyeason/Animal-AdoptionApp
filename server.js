0f314afa876fcc4b914c87243650c4b736b03868
chrissyeason <chrissyeason@gmail.com>
2019-07-23 20:38:27 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
-const mongoURI = 'mongodb://localhost:27017/'+'Animals';
-// const session = require('express-session');
+const methodOverride = require('method-override');
+const mongoURI = 'mongodb://localhost:27017/'+'animals';
+const userController = require('./controllers/userController.js')
+const session = require('express-session');
 mongoose.connect(mongoURI, { useNewUrlParser: true }, () =>{
     console.log("the connection with mongodb is established")
-});
-const catController = require('./controllers/catController');
+ });
 
-// MIDDLEWARE
 app.use(methodOverride('_method')); //For put and Delete
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/cats', catController);
-
-
-// app.use(session({
-//     secret: "SecretStuff",
-//     resave:false,
-//     saveUninitialized:false
-// }))
+app.use("/user", userController)
+app.use(session({
+    secret: "SecretStuff",
+    resave:false,
+    saveUninitialized:false
+}))
 
 app.get('/', (req, res) =>{
-    // res.send("we working!!!")
     res.render('index.ejs');
 })
 
